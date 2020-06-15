f98a0b83c4823a2f03a57c4d36854f5543e1d7f6
caio-cruz-garcia <61151058+caio-cruz-garcia@users.noreply.github.com>
2020-06-15 11:31:40 -0300

    Menu.java
    criacao de pacotes, Enum,models,Interface e criacao de um menu

---
+++ src/com/company/Menu.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Menu {
+
+    public static void run(){
+
+    }
+}
---
+++ src/com/company/Models/Usuario.java
@@ -0,0 +1,46 @@
+package com.company.Models;
+
+public class Usuario {
+    private String name;
+    private String email;
+    private String senha;
+
+    public Usuario(String name, String email, String senha) {
+        this.name = name;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", senha='" + senha + '\'' +
+                '}';
+    }
+}
