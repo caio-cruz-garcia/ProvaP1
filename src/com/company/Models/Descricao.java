de7287bf18dc47c3a31c9162f10ebcacea107a73
caio-cruz-garcia <61151058+caio-cruz-garcia@users.noreply.github.com>
2020-06-24 20:47:37 -0300

    Update Descricao.java
    um pouco da descrição

--- src/com/company/Models/Descricao.java
+++ src/com/company/Models/Descricao.java
@@ -1,4 +1,35 @@
 package com.company.Models;
 
-public class Descricao {
+import com.company.Enum.Status;
+import com.company.Interface.AlterarStatus;
+import com.company.Interface.VerStatus;
+
+import java.util.List;
+
+public class Descricao implements VerStatus, AlterarStatus {
+    private List<VerificarPedidos> verificarPedidosList;
+    private List<AlterarPedidos> alterarPedidosList;
+
+    public Descricao(List<VerificarPedidos> verificarPedidosList, List<AlterarPedidos> alterarPedidosList) {
+        this.verificarPedidosList = verificarPedidosList;
+        this.alterarPedidosList = alterarPedidosList;
+    }
+
+    public List<VerificarPedidos> getVerificarPedidosList() {
+        return verificarPedidosList;
+    }
+
+    public List<AlterarPedidos> getAlterarPedidosList() {
+        return alterarPedidosList;
+    }
+
+    @Override
+    public Status alterarStatus(List<AlterarPedidos> alterarStatus) {
+        return null;
+    }
+
+    @Override
+    public Status checkStatus(List<VerificarPedidos> verStatus) {
+        return null;
+    }
 }
