c2c1db20d9a82fc2005c7e2d29e5ccb56eaf0426
caio-cruz-garcia <61151058+caio-cruz-garcia@users.noreply.github.com>
2020-06-25 05:23:07 -0300

    Update Tipos.java

--- src/com/company/Enum/Tipos.java
+++ src/com/company/Enum/Tipos.java
@@ -1,7 +1,7 @@
 package com.company.Enum;
 
 /**
- * * Representação dos tipos de sabores da pizza por uma classe Enum.
+ * Representação dos tipos de sabores da pizza por uma classe Enum.
  * @author Caio Cruz Alfonso Garcia - caiogarcia96@hotmail.com
  * @since 25/06/2020
  * @version 1.0
